a34819316bc5fe5d0fa59f2f2865a4957aaa3cba
Stefano Salvatori <ssalvatori@udec.cl>
2010-06-21 16:39:08 -0400

    Hits

--- controllers/murls_controller.php
+++ controllers/murls_controller.php
@@ -1,18 +1,18 @@
 <?php
+
 class MurlsController extends AppController {
 
-    var $name       = 'Murls';
-
-
-    var $paginate   = array(
-            'limit' => 10,
-            'order' => array(
-                            'Murl.id' => 'desc'
-            )
+    var $name = 'Murls';
+    var $paginate = array(
+        'limit' => 10,
+        'order' => array(
+            'Murl.id' => 'desc'
+        )
     );
 
     function add() {
         if ($this->data) {
+
             $this->data['Murl']['remote'] = $this->RequestHandler->getClientIP();
             $this->data['Murl']['referer'] = $this->RequestHandler->getReferer();
             $this->data['Murl']['agent'] = $_SERVER['HTTP_USER_AGENT'];
@@ -20,26 +20,26 @@ class MurlsController extends AppController {
             if ($this->Murl->validates($this->data)) {
 
                 /* reverse murl */
-                $found = stristr($this->data['Murl']['uri'],'http://murl.net/');
+                $found = stristr($this->data['Murl']['uri'], 'http://murl.net/');
                 if ($found) {
-                    $this->redirect("/reverse/".substr($found, strlen('http://murl.net/')));
+                    $this->redirect("/reverse/" . substr($found, strlen('http://murl.net/')));
                     exit(); // ta de más
                 }
 
                 $result = $this->Murl->find('first', array('conditions' => array('Murl.uri =' => $this->data['Murl']['uri'])));
                 if ($result) {
                     $this->Session->setFlash('Entry already exists.');
-                    $this->set('code',$result['Murl']['code']);
+                    $this->set('code', $result['Murl']['code']);
                 } else {
                     $this->Murl->create();
-                    if ($this->Murl->save($this->data)) {
+                    if ($this->Murl->saveAll($this->data)) {
                         $id = $this->Murl->getInsertId();
                         $code = $this->Murl->genCode($id);
                         $delta = $this->Murl->getDelta($id);
                         $crunch_msg = $this->Murl->getCrunch($delta);
-                        $this->Session->setFlash($crunch_msg.' Savings of '.$delta.' characters.');
+                        $this->Session->setFlash($crunch_msg . ' Savings of ' . $delta . ' characters.');
 
-                        $this->set('code',$code);
+                        $this->set('code', $code);
                     } else {
                         $this->Session->setFlash("Error: Unable to save");
                     }
@@ -51,20 +51,20 @@ class MurlsController extends AppController {
     }
 
     function domain() {
-        $all_murls  = $this->Murl->find('all', array('order' => array('Murl.id DESC')));
+        $all_murls = $this->Murl->find('all', array('order' => array('Murl.id DESC')));
         foreach ($all_murls as $one_murl) {
-            $durl=urldecode(stripslashes($one_murl['Murl']['uri']));
-            $domain=parse_url($durl, PHP_URL_HOST);
-            $bits=array_reverse(explode(".", $domain));
-            error_reporting (0);
+            $durl = urldecode(stripslashes($one_murl['Murl']['uri']));
+            $domain = parse_url($durl, PHP_URL_HOST);
+            $bits = array_reverse(explode(".", $domain));
+            error_reporting(0);
             if ($bits[0] && $bits[1]) {
-                $domain=$bits[1] . "." . $bits[0];
+                $domain = $bits[1] . "." . $bits[0];
                 $domain_hash[$domain]++;
             }
-            error_reporting (E_ALL  & ~E_NOTICE);
+            error_reporting(E_ALL & ~E_NOTICE);
         }
         asort($domain_hash, SORT_NUMERIC);
-        $domain_hash=array_reverse($domain_hash);
+        $domain_hash = array_reverse($domain_hash);
         $this->set('domains', $domain_hash);
     }
 
@@ -72,7 +72,7 @@ class MurlsController extends AppController {
         $code = $this->params['url']['url'];
         $this->set('error', 0);
         $this->set('code', $code);
-        $result=$this->Murl->find('first', array('conditions' => array('Murl.code =' => $code)));
+        $result = $this->Murl->find('first', array('conditions' => array('Murl.code =' => $code)));
 
         if ($result) {
             if ($result['Murl']['destruct'] && $result['Murl']['hits']) {
@@ -85,7 +85,15 @@ class MurlsController extends AppController {
                 $this->Session->setFlash('Bad Password. Try again?');
                 $this->set('error', '401');
             } else {
-                $this->Murl->updateAll(array('Murl.hits'=>'Murl.hits+1'), array('Murl.id'=>$result['Murl']['id']));
+                $this->Murl->updateAll(array('Murl.hits' => 'Murl.hits+1'), array('Murl.id' => $result['Murl']['id']));
+
+                $this->data['Hit']['murl_id'] = $result['Murl']['id'];
+                $this->data['Hit']['remote'] = $this->RequestHandler->getClientIP();
+                $this->data['Hit']['referer'] = $this->RequestHandler->getReferer();
+                $this->data['Hit']['agent'] = $_SERVER['HTTP_USER_AGENT'];
+
+                $this->Murl->Hit->save($this->data);
+
                 $this->redirect(urldecode(stripslashes($result['Murl']['uri'])));
                 exit();
             }
@@ -99,15 +107,15 @@ class MurlsController extends AppController {
         $this->set('title_for_layout', "Random mURLs");
         $this->Murl->recursive = 0;
         $this->set('murls', $this->Murl->find('all', array(
-                'limit' => 1,
-                'order' => array('rand()'),
-        )));
+                    'limit' => 1,
+                    'order' => array('rand()'),
+                )));
     }
 
     function reverse() {
         $this->set('title_for_layout', "Reverse mURL");
-        $result = $this->Murl->find("first",array('conditions'=>array('Murl.code'=>$this->params["code"],'Murl.destruct'=>0)));
-        $this->set('murl',$result);
+        $result = $this->Murl->find("first", array('conditions' => array('Murl.code' => $this->params["code"], 'Murl.destruct' => 0)));
+        $this->set('murl', $result);
     }
 
     function search() {
@@ -115,31 +123,32 @@ class MurlsController extends AppController {
         $this->Murl->recursive = 0;
         if ($this->data) {
             # Quiero poner esto en un modelo para poder usar el validate.
-            if (strlen($this->data['Murl']['field']) < 3 ) {
+            if (strlen($this->data['Murl']['field']) < 3) {
                 $this->Session->setFlash('Too short a criteria. Minimum of 3 characters required');
             } else {
                 $this->set('murls', $this->Murl->find('all', array(
-                        'limit' => 20,
-                        'order' => array('Murl.id DESC'),
-                        'conditions' => array('Murl.uri LIKE' => '%'.$this->data['Murl']['field'].'%')
-                )));
+                            'limit' => 20,
+                            'order' => array('Murl.id DESC'),
+                            'conditions' => array('Murl.uri LIKE' => '%' . $this->data['Murl']['field'] . '%')
+                        )));
             }
         }
     }
 
     function top() {
         $this->set('title_for_layout', "Top mURLs");
-        $this->Murl->recursive = 0 ;
+        $this->Murl->recursive = 0;
         $this->set('murls', $this->Murl->find('all', array(
-                'limit' => 20,
-                'order' => array('Murl.hits DESC'),
-        )));
+                    'limit' => 20,
+                    'order' => array('Murl.hits DESC'),
+                )));
     }
 
     function view() {
         $this->set('title_for_layout', "View mURLs");
         $this->Murl->recursive = 0;
-        $this->set('murls',$this->paginate());
+        $this->set('murls', $this->paginate());
     }
+
 }
 ?>
--- models/murl.php
+++ models/murl.php
@@ -1,47 +1,51 @@
 <?php
+
 class Murl extends AppModel {
+
     var $name = 'Murl';
     var $displayField = 'code';
     var $validate = array(
-            'uri' => array(
-                            'rule' => 'url',
-                            'required' => 'true',
-                            'message' => 'Please provide an URL to crunch.'
-            )
+        'uri' => array(
+            'rule' => 'url',
+            'required' => 'true',
+            'message' => 'Please provide an URL to crunch.'
+        )
     );
 
+    var $hasMany = array('Hit');
+
     function genCode($id) {
-        $code = base_convert($id,10,36);
+        $code = base_convert($id, 10, 36);
         $this->id = $id;
-        $this->saveField('code',$code);
+        $this->saveField('code', $code);
         return $code;
-        
     }
 
     function getCrunch($delta) {
-        $crunch_msg="What's going on?";
+        $crunch_msg = "What's going on?";
         if ($delta < 0) {
-            $crunch_msg="We're ashamed... good for you.";
+            $crunch_msg = "We're ashamed... good for you.";
         } elseif ($delta == 0) {
-            $crunch_msg="Wow, we kinnda broke even, uh?";
+            $crunch_msg = "Wow, we kinnda broke even, uh?";
         } elseif ($delta == 1) {
-            $crunch_msg="One is better than none.";
-        } elseif ( ($delta > 1) && ($delta < 20) ) {
-            $crunch_msg="You got shrunken!";
-        } elseif ( ($delta >= 20) && ($delta < 50) ) {
-            $crunch_msg="Now that's good shrinking.";
-        } elseif ( ($delta >= 50) && ($delta < 150) ) {
-            $crunch_msg="Massive shrinkage detected!";
+            $crunch_msg = "One is better than none.";
+        } elseif (($delta > 1) && ($delta < 20)) {
+            $crunch_msg = "You got shrunken!";
+        } elseif (($delta >= 20) && ($delta < 50)) {
+            $crunch_msg = "Now that's good shrinking.";
+        } elseif (($delta >= 50) && ($delta < 150)) {
+            $crunch_msg = "Massive shrinkage detected!";
         } elseif ($delta >= 150) {
-            $crunch_msg="You are the King of mURL!";
+            $crunch_msg = "You are the King of mURL!";
         }
         return $crunch_msg;
     }
 
     function getDelta($id) {
-        $result = $this->find('first',array('conditions'=>array('Murl.id'=>$id)));
+        $result = $this->find('first', array('conditions' => array('Murl.id' => $id)));
 
-        return strlen($result['Murl']['uri'])-(strlen($result['Murl']['code'])+strlen('http://murl.net/'));
+        return strlen($result['Murl']['uri']) - (strlen($result['Murl']['code']) + strlen('http://murl.net/'));
     }
+
 }
 ?>
